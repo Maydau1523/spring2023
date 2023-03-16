938f530483c13926a17ad5fc09cc718ef650242e
Maydau1524 <126518754+Maydau1523@users.noreply.github.com>
2023-03-16 00:02:40 -0600

    Ejercicio 11/12
    PIng.

---
+++ excercise11/excercise11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+int Suma(int x, int y)
+{
+    return x + y;
+}
+
+int Resta(int x, int y)
+{
+    return x - y;
+}
+
+int Multiplica(int x, int y)
+{
+    return x * y;
+}
+
+void Print(int resultado)
+{
+    Console.WriteLine("Resultado = {0}", resultado);
+}
+
+// El código principal que utiliza las funciones declaradas arriba
+int resultado = Suma(6, 7);
+Print(resultado);
+
+resultado = Resta(5, 6);
+Print(resultado);
+{
+
+}
+
+
+
+
+
+
---
+++ excercise11/excersise12/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int num1;
+int num2;
+
+int num3;
+
+Console.WriteLine("Introduce numero 1");
+num1 = Int32.Parse( Console.ReadLine());
+
+Console.WriteLine("Introduce numero 2");
+num2 = Int32.Parse( Console.ReadLine());
+
+
+Console.WriteLine("Introduce numero 3");
+num3 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Suma de {0}", num1+ num2+ num3);
